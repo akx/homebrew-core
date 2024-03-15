72e1d842c034b1674b5f98ec6a7860141cdd71f7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-03-16 02:01:54 +0800

    csound: bump revision (llvm 18.1.1)

--- Formula/c/csound.rb
+++ Formula/c/csound.rb
@@ -1,12 +1,22 @@
 class Csound < Formula
   desc "Sound and music computing system"
   homepage "https://csound.com"
-  url "https://github.com/csound/csound.git",
-      tag:      "6.18.1",
-      revision: "a1580f9cdf331c35dceb486f4231871ce0b00266"
   license "LGPL-2.1-or-later"
-  revision 6
+  revision 7
   head "https://github.com/csound/csound.git", branch: "master"
+
+  # Remove `stable` block when patch is no longer needed
+  stable do
+    url "https://github.com/csound/csound.git",
+        tag:      "6.18.1",
+        revision: "a1580f9cdf331c35dceb486f4231871ce0b00266"
+
+    # LLVM 18 support
+    patch do
+      url "https://github.com/csound/csound/commit/2a071ae8ca89bc21b5c80037f8c95a01bb670ac9.patch?full_index=1"
+      sha256 "c7026330b5c89ab399e74aff17019067705011b7e35b9c75f9ed1a5878f53b4b"
+    end
+  end
 
   livecheck do
     url :stable
